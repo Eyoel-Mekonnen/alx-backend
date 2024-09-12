d9c1cc4cd04df932264017e59df45464cfd5020e
root <root@DESKTOP-B19RQ5P>
2024-09-12 14:40:33 +0300

    app config and bable instance

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Set up basic Babel."""
+from flask_babel import Babel
+from flask import Flask, render_template
+
+
+class Config:
+    """Language class."""
+
+    LANGUAGES = ["en", "fr"]
+    babel_default_locale = "en"
+    timezone = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/')
+def home():
+    """Render and return html."""
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
